e4355f1813d9a595d7a24bc38d7d5e6bcb04bfa1
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-03-03 23:56:03 -0800

    [UPDATE] Create anagram dictionary

--- Code/jumble.py
+++ Code/jumble.py
@@ -1,21 +1,35 @@
 
+from pprint import pprint
 import random
-import sys
+from load_dictionary import read_in_data
 
-dict_words = []
-anagrams = []
+jumble_dict = dict()
 
+def unjumble_key(word):
+    return "".join(sorted(word))
 
-def find_anagrams(words):
+def make_unjumble_dict(words):
+    """Create a jumbled key for every word in 'dict' file & append its anagrams as values []"""
     for word in words:
-        for other_word in words:
-            if sorted(word) == sorted(other_word):
-                anagrams.append(word)
-                anagrams.append(other_word)
+        sort_wrd = unjumble_key(word)
+        jumble_dict.setdefault(sort_wrd, []).append(word)
+
+def unjumble(word):
+    return jumble_dict[word]
 
 
-if __name__ == '__main__':
-    dict_words = read_in_data('/usr/share/dict/words')
-    # print(words)
-    find_anagrams(dict_words)
-    print(anagrams)
+# def random_words(words, num_words):
+#     rand_w = []
+#     for i in range(num_words):
+#         rand_i = random.randrange(len(words) - 1)
+#         rand_w.append(words[rand_i])
+
+
+if __name__ == "__main__":
+    # '/usr/share/dict/words'
+    # ['cat', 'car', 'listen', 'taco', 'silent']
+    # arr = read_in_data('/usr/share/dict/words')
+    make_unjumble_dict(['cat', 'car', 'shrub', 'rats', 'star', 'arts', 'regal', 'listen', 'taco', 'silent', 'brush'])
+    pprint(jumble_dict)
+
+
