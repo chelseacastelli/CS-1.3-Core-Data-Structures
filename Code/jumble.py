9419f47395d997773aa0647c4e7b9a32e67126ac
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-03-04 20:49:28 -0800

    [UPDATE] Takes in input, solves words, & displays to terminal

--- Code/jumble.py
+++ Code/jumble.py
@@ -1,35 +1,49 @@
 
-from pprint import pprint
-import random
 from load_dictionary import read_in_data
+import os
+from subprocess import call
 
 jumble_dict = dict()
 
 def unjumble_key(word):
+    """Return a string that is the unjumble key of its input"""
     return "".join(sorted(word))
 
 def make_unjumble_dict(words):
     """Create a jumbled key for every word in 'dict' file & append its anagrams as values []"""
     for word in words:
+        word = word.lower()
         sort_wrd = unjumble_key(word)
         jumble_dict.setdefault(sort_wrd, []).append(word)
 
 def unjumble(word):
-    return jumble_dict[word]
+    """Returns a list of all words in the dictionary to which the input string unjumbles"""
+    return jumble_dict["".join(sorted(word))]
 
 
-# def random_words(words, num_words):
-#     rand_w = []
-#     for i in range(num_words):
-#         rand_i = random.randrange(len(words) - 1)
-#         rand_w.append(words[rand_i])
+def clear():
+    '''A function that clears the terminal for better readability'''
+    _ = call('clear' if os.name =='posix' else 'cls')
 
 
 if __name__ == "__main__":
-    # '/usr/share/dict/words'
-    # ['cat', 'car', 'listen', 'taco', 'silent']
-    # arr = read_in_data('/usr/share/dict/words')
-    make_unjumble_dict(['cat', 'car', 'shrub', 'rats', 'star', 'arts', 'regal', 'listen', 'taco', 'silent', 'brush'])
-    pprint(jumble_dict)
+    words = []
 
+    arr = read_in_data('/usr/share/dict/words')
+    make_unjumble_dict(arr)
 
+    print("-----WELCOME TO JUMBLE!-----\n")
+
+    num = input("How many words are you going to enter? ")
+
+    for _ in range(int(num)):
+        words.append(input("Please input a jumble word: "))
+
+    clear()
+
+    print("\n-----SOLVED!-----\n")
+
+    for word in words:
+        print(f'{word} --> {unjumble(word)[0]}')
+
+    print('\n')
