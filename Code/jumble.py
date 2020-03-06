89cc9b7d8d1aff18cda45d02c55a28e2e0403dbc
chelseacastelli <chelsea.castelli@students.makeschool.com>
2020-03-05 16:23:40 -0800

    [UPDATE] Refactor using OOP

--- Code/jumble.py
+++ Code/jumble.py
@@ -3,47 +3,51 @@ from load_dictionary import read_in_data
 import os
 from subprocess import call
 
-jumble_dict = dict()
 
-def unjumble_key(word):
-    """Return a string that is the unjumble key of its input"""
-    return "".join(sorted(word))
+class DictUnjumble(object):
+    def __init__(self):
+        self.dictionary = {}
 
-def make_unjumble_dict(words):
-    """Create a jumbled key for every word in 'dict' file & append its anagrams as values []"""
-    for word in words:
-        word = word.lower()
-        sort_wrd = unjumble_key(word)
-        jumble_dict.setdefault(sort_wrd, []).append(word)
+    def unjumble_key(self, word):
+        """Return a string that is the unjumble key of its input"""
+        return "".join(sorted(word))
 
-def unjumble(word):
-    """Returns a list of all words in the dictionary to which the input string unjumbles"""
-    return jumble_dict["".join(sorted(word))]
+    def make_unjumble_dict(self, words):
+        """Create a jumbled key for every word in 'dict' file & append its anagrams as values []"""
+        for word in words:
+            word = word.lower()
+            sort_wrd = self.unjumble_key(word)
+            self.dictionary.setdefault(sort_wrd, []).append(word)
+
+    def unjumble(self, word):
+        """Returns a list of all words in the dictionary to which the input string unjumbles"""
+        return self.dictionary["".join(sorted(word)).lower()]
 
 
 def clear():
-    '''A function that clears the terminal for better readability'''
+    """A function that clears the terminal for better readability"""
     _ = call('clear' if os.name =='posix' else 'cls')
 
 
 if __name__ == "__main__":
-    words = []
+    jumble = DictUnjumble()
+    inputted_words = []
 
-    arr = read_in_data('/usr/share/dict/words')
-    make_unjumble_dict(arr)
+    dict_words = read_in_data('/usr/share/dict/words')
+    jumble.make_unjumble_dict(dict_words)
 
     print("-----WELCOME TO JUMBLE!-----\n")
-
     num = input("How many words are you going to enter? ")
+    print('\n')
 
     for _ in range(int(num)):
-        words.append(input("Please input a jumble word: "))
+        inputted_words.append(input("Please input a jumble word: "))
 
     clear()
 
     print("\n-----SOLVED!-----\n")
 
-    for word in words:
-        print(f'{word} --> {unjumble(word)[0]}')
+    for word in inputted_words:
+        print(f'{word} --> {jumble.unjumble(word)[0]}')
 
     print('\n')
